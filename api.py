139848086c6f0099fb2b148330a3b912f292e4c8
Gerard <gerardpoukpezi@gmail.com>
2023-05-04 18:51:37 +0000

    first commit

--- api.py
+++ api.py
@@ -57,7 +57,7 @@ with app.app_context():
 def get_articles():
     all_articles = Article.query.all()
     result = articles_schema.dump(all_articles)
-    return jsonify(result), 200
+    return jsonify(result)
 
 # Endpoint pour consulter un article par son id
 @app.route('/articles/<id>', methods=['GET'])
@@ -83,6 +83,68 @@ def add_article():
         db.session.rollback()
         return jsonify({'message': 'Erreur: cet article existe déjà.'}), 409
 
+@app.route('/articles/<int:id>', methods=['PUT'])
+def update_article(id):
+    article = Article.query.get(id)
+    if article:
+        nom = request.json.get('nom', article.nom)
+        desc = request.json.get('desc', article.desc)
+        prix = request.json.get('prix', article.prix)
+        quantite = request.json.get('quantite', article.quantite)
+        categorie_id = request.json.get('categorie_id', article.categorie_id)
+
+        article.nom = nom
+        article.desc = desc
+        article.prix = prix
+        article.quantite = quantite
+        article.categorie_id = categorie_id
+
+        db.session.commit()
+
+        return jsonify({'message': 'Article modifié avec succès.'})
+    else:
+        return jsonify({'message': 'Article non trouvé.'}), 404
+
+
+@app.route('/articles/<int:id>', methods=['DELETE'])
+def delete_article(id):
+    article = Article.query.get(id)
+    if article:
+        db.session.delete(article)
+        db.session.commit()
+
+        return jsonify({'message': 'Article supprimé avec succès.'})
+    else:
+        return jsonify({'message': 'Article non trouvé.'}), 404
+
+
+@app.route('/articles/recherche', methods=['GET'])
+def search_articles():
+    keyword = request.args.get('q')
+    articles = Article.query.filter(Article.nom.contains(keyword) | Article.description.contains(keyword)).all()
+
+    if articles:
+        result = []
+        for article in articles:
+            data = {}
+            data['id'] = article.id
+            data['nom'] = article.nom
+            data['desc'] = article.desc
+            data['prix'] = article.prix
+            data['quantite'] = article.quantite
+            data['categorie'] = article.categorie.nom
+            result.append(data)
+
+        return jsonify(result)
+    else:
+        return jsonify({'message': 'Aucun article trouvé.'}), 404
+@app.route('/articles/search', methods=['GET'])
+def search_article():
+    keyword = request.args.get('q', '') # récupère le mot-clé depuis les paramètres de requête
+    articles = Article.query.filter(Article.nom.ilike('%' + keyword + '%')).all() # effectue la recherche
+    if not articles:
+        return jsonify({'message': 'Aucun article trouvé.'}), 404
+    return jsonify(articles_schema.dump(articles)), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
